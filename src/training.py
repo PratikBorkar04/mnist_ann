d2eb0e6a21622718a64f1f5891e16b00fea18ffc
Pratik Borkar <pratikab01@gmail.com>
2024-03-05 16:19:45 +0530

    Model Training completed 16.19 5/3/24

--- src/training.py
+++ src/training.py
@@ -6,12 +6,25 @@ sys.path.append(parent_dir)
 
 from src.utils.common import read_config
 from src.utils.data_mgmt import get_data
+from src.utils.model import create_model
 import argparse
 
 def training(config_path):
     config = read_config(config_path)
+
     validation_datasize = config['params']["validation_datasize"]
     (X_train,y_train),(X_valid,y_valid),(X_test,y_test) = get_data(validation_datasize)
+
+    LOSS_FUNCTION = config['params']["loss_function"]
+    OPTIMIZER = config['params']["optimizer"]
+    METRICES = config['params']["metrics"]
+    NUM_CLASSES = config['params']["num_classes"]
+    EPOCHS = config['params']["epochs"]
+
+
+    model = create_model(LOSS_FUNCTION,OPTIMIZER,METRICES,NUM_CLASSES)
+    VALIDATION_SET = (X_valid,y_valid)
+    history = model.fit(X_train,y_train,epochs = EPOCHS,validation_data=VALIDATION_SET)
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
